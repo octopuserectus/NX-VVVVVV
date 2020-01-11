be947afc9c1b30527f8fe5ce41f91b9a17c6aa4c
Nichole Mattera <697668+NicholeMattera@users.noreply.github.com>
2020-01-10 20:31:31 -0500

    Removed more unneeded code from the switch port.

--- desktop_version/src/main.cpp
+++ desktop_version/src/main.cpp
@@ -42,9 +42,9 @@ edentities edentity[3000];
 
 editorclass ed;
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-    if(!FILESYSTEM_init(argv[0]))
+    if (!FILESYSTEM_init(argv[0]))
     {
         return 1;
     }
@@ -61,7 +61,10 @@ int main(int argc, char *argv[])
         SDL_SetHintWithPriority(SDL_HINT_RENDER_DRIVER, argv[2], SDL_HINT_OVERRIDE);
     }
 
-    NETWORK_init();
+    // Switch doesn't have Steam.
+    #if !defined(__SWITCH__)
+        NETWORK_init();
+    #endif
 
     Screen gameScreen;
 
@@ -124,7 +127,6 @@ int main(int argc, char *argv[])
     SDL_SetSurfaceBlendMode(graphics.backBuffer, SDL_BLENDMODE_NONE);
     graphics.Makebfont();
 
-
     graphics.forgroundBuffer =  SDL_CreateRGBSurface(SDL_SWSURFACE ,320 ,240 ,fmt->BitsPerPixel,fmt->Rmask,fmt->Gmask,fmt->Bmask,fmt->Amask  );
     SDL_SetSurfaceBlendMode(graphics.forgroundBuffer, SDL_BLENDMODE_NONE);
 
@@ -147,11 +149,11 @@ int main(int argc, char *argv[])
     KeyPoll key;
     mapclass map;
 
-    map.ypos = (700-29) * 8;
+    map.ypos = (700 - 29) * 8;
     map.bypos = map.ypos / 2;
 
-    //Moved screensetting init here from main menu V2.1
     game.loadstats(map, graphics);
+
     if (game.usingmmmmmm == 0) music.usingmmmmmm = false;
     if (game.usingmmmmmm == 1) music.usingmmmmmm = true;
     if (game.slowdown == 0) game.slowdown = 30;
@@ -178,34 +180,37 @@ int main(int argc, char *argv[])
             break;
     }
 
-    //Check to see if you've already unlocked some achievements here from before the update
-    if (game.swnbestrank > 0)
-    {
-        if (game.swnbestrank >= 1) NETWORK_unlockAchievement("vvvvvvsupgrav5");
-        if (game.swnbestrank >= 2) NETWORK_unlockAchievement("vvvvvvsupgrav10");
-        if (game.swnbestrank >= 3) NETWORK_unlockAchievement("vvvvvvsupgrav15");
-        if (game.swnbestrank >= 4) NETWORK_unlockAchievement("vvvvvvsupgrav20");
-        if (game.swnbestrank >= 5) NETWORK_unlockAchievement("vvvvvvsupgrav30");
-        if (game.swnbestrank >= 6) NETWORK_unlockAchievement("vvvvvvsupgrav60");
-    }
+    // Switch doesn't have Steam.
+    #if !defined(__SWITCH__)
+        //Check to see if you've already unlocked some achievements here from before the update
+        if (game.swnbestrank > 0)
+        {
+            if (game.swnbestrank >= 1) NETWORK_unlockAchievement("vvvvvvsupgrav5");
+            if (game.swnbestrank >= 2) NETWORK_unlockAchievement("vvvvvvsupgrav10");
+            if (game.swnbestrank >= 3) NETWORK_unlockAchievement("vvvvvvsupgrav15");
+            if (game.swnbestrank >= 4) NETWORK_unlockAchievement("vvvvvvsupgrav20");
+            if (game.swnbestrank >= 5) NETWORK_unlockAchievement("vvvvvvsupgrav30");
+            if (game.swnbestrank >= 6) NETWORK_unlockAchievement("vvvvvvsupgrav60");
+        }
 
-    if (game.unlock[5]) NETWORK_unlockAchievement("vvvvvvgamecomplete");
-    if (game.unlock[19]) NETWORK_unlockAchievement("vvvvvvgamecompleteflip");
-    if (game.unlock[20]) NETWORK_unlockAchievement("vvvvvvmaster");
+        if (game.unlock[5]) NETWORK_unlockAchievement("vvvvvvgamecomplete");
+        if (game.unlock[19]) NETWORK_unlockAchievement("vvvvvvgamecompleteflip");
+        if (game.unlock[20]) NETWORK_unlockAchievement("vvvvvvmaster");
 
-    if (game.bestgamedeaths > -1) {
-        if (game.bestgamedeaths <= 500) NETWORK_unlockAchievement("vvvvvvcomplete500");
-        if (game.bestgamedeaths <= 250) NETWORK_unlockAchievement("vvvvvvcomplete250");
-        if (game.bestgamedeaths <= 100) NETWORK_unlockAchievement("vvvvvvcomplete100");
-        if (game.bestgamedeaths <= 50) NETWORK_unlockAchievement("vvvvvvcomplete50");
-    }
+        if (game.bestgamedeaths > -1) {
+            if (game.bestgamedeaths <= 500) NETWORK_unlockAchievement("vvvvvvcomplete500");
+            if (game.bestgamedeaths <= 250) NETWORK_unlockAchievement("vvvvvvcomplete250");
+            if (game.bestgamedeaths <= 100) NETWORK_unlockAchievement("vvvvvvcomplete100");
+            if (game.bestgamedeaths <= 50) NETWORK_unlockAchievement("vvvvvvcomplete50");
+        }
 
-    if (game.bestrank[0] >= 3) NETWORK_unlockAchievement("vvvvvvtimetrial_station1_fixed");
-    if (game.bestrank[1] >= 3) NETWORK_unlockAchievement("vvvvvvtimetrial_lab_fixed");
-    if (game.bestrank[2] >= 3) NETWORK_unlockAchievement("vvvvvvtimetrial_tower_fixed");
-    if (game.bestrank[3] >= 3) NETWORK_unlockAchievement("vvvvvvtimetrial_station2_fixed");
-    if (game.bestrank[4] >= 3) NETWORK_unlockAchievement("vvvvvvtimetrial_warp_fixed");
-    if (game.bestrank[5] >= 3) NETWORK_unlockAchievement("vvvvvvtimetrial_final_fixed");
+        if (game.bestrank[0] >= 3) NETWORK_unlockAchievement("vvvvvvtimetrial_station1_fixed");
+        if (game.bestrank[1] >= 3) NETWORK_unlockAchievement("vvvvvvtimetrial_lab_fixed");
+        if (game.bestrank[2] >= 3) NETWORK_unlockAchievement("vvvvvvtimetrial_tower_fixed");
+        if (game.bestrank[3] >= 3) NETWORK_unlockAchievement("vvvvvvtimetrial_station2_fixed");
+        if (game.bestrank[4] >= 3) NETWORK_unlockAchievement("vvvvvvtimetrial_warp_fixed");
+        if (game.bestrank[5] >= 3) NETWORK_unlockAchievement("vvvvvvtimetrial_final_fixed");
+    #endif
 
     entityclass obj;
     obj.init();
@@ -288,131 +293,136 @@ int main(int argc, char *argv[])
             game.infocus = true;
         #else
             game.infocus = key.isActive;
+
+            if (!game.infocus)
+            {
+                if (game.getGlobalSoundVol() > 0)
+                {
+                    game.setGlobalSoundVol(0);
+                }
+                FillRect(graphics.backBuffer, 0x00000000);
+                graphics.bprint(5, 110, "Game paused", 196 - help.glow, 255 - help.glow, 196 - help.glow, true);
+                graphics.bprint(5, 120, "[click to resume]", 196 - help.glow, 255 - help.glow, 196 - help.glow, true);
+                graphics.bprint(5, 230, "Press M to mute in game", 164 - help.glow, 196 - help.glow, 164 - help.glow, true);
+                graphics.render();
+                //We are minimised, so lets put a bit of a delay to save CPU
+                SDL_Delay(100);
+            }
+            else
+            {
         #endif
 
-        if (!game.infocus)
+        switch (game.gamestate)
         {
-            if (game.getGlobalSoundVol() > 0)
-            {
-                game.setGlobalSoundVol(0);
-            }
-            FillRect(graphics.backBuffer, 0x00000000);
-            graphics.bprint(5, 110, "Game paused", 196 - help.glow, 255 - help.glow, 196 - help.glow, true);
-            graphics.bprint(5, 120, "[click to resume]", 196 - help.glow, 255 - help.glow, 196 - help.glow, true);
-            graphics.bprint(5, 230, "Press M to mute in game", 164 - help.glow, 196 - help.glow, 164 - help.glow, true);
-            graphics.render();
-            //We are minimised, so lets put a bit of a delay to save CPU
-            SDL_Delay(100);
+            case PRELOADER:
+                //Render
+                preloaderrender(graphics, game, help);
+                break;
+
+            case EDITORMODE:
+                graphics.flipmode = false;
+                //Input
+                editorinput(key, graphics, game, map, obj, help, music);
+                //Render
+                editorrender(key, graphics, game, map, obj, help);
+                //Logic
+                editorlogic(key, graphics, game, obj, music, map, help);
+                break;
+
+            case TITLEMODE:
+                //Input
+                titleinput(key, graphics, map, game, obj, help, music);
+                //Render
+                titlerender(graphics, map, game, obj, help, music);
+                //Logic
+                titlelogic(graphics, game, obj, help, music, map);
+                break;
+
+            case GAMEMODE:
+                if (map.towermode)
+                {
+                    //Input
+                    gameinput(key, graphics, game, map, obj, help, music);
+                    //Render
+                    towerrender(graphics, game, map, obj, help);
+                    //Logic
+                    towerlogic(graphics, game, obj, music, map, help);
+                }
+                else
+                {
+                    if (script.running)
+                    {
+                        script.run(key, graphics, game, map, obj, help, music);
+                    }
+
+                    //Input
+                    gameinput(key, graphics, game, map, obj, help, music);
+                    //Render
+                    gamerender(graphics,map, game, obj, help);
+                    //Logic
+                    gamelogic(graphics, game, obj, music, map,  help);
+                }
+                break;
+
+            case MAPMODE:
+                //Input
+                mapinput(key, graphics, game, map, obj, help, music);
+                //Render
+                maprender(graphics, game, map, obj, help);
+                //Logic
+                maplogic(graphics, game, obj ,music , map, help );
+                break;
+
+            case TELEPORTERMODE:
+                //Input
+                if(game.useteleporter)
+                {
+                    teleporterinput(key, graphics, game, map, obj, help, music);
+                }
+                else
+                {
+                    if (script.running)
+                    {
+                        script.run(key, graphics, game, map, obj, help, music);
+                    }
+
+                    gameinput(key, graphics, game, map, obj, help, music);
+                }
+                //Render
+                teleporterrender(graphics, game, map, obj, help);
+                //Logic
+                maplogic(graphics, game, obj, music, map, help);
+                break;
+
+            case GAMECOMPLETE:
+                //Input
+                gamecompleteinput(key, graphics, game, map, obj, help, music);
+                //Render
+                gamecompleterender(graphics, game, obj, help, map);
+                //Logic
+                gamecompletelogic(graphics, game, obj, music, map, help);
+                break;
+
+            case GAMECOMPLETE2:
+                //Input
+                gamecompleteinput2(key, graphics, game, map, obj, help, music);
+                //Render
+                gamecompleterender2(graphics, game, obj, help);
+                //Logic
+                gamecompletelogic2(graphics, game, obj, music, map, help);
+                break;
+
+            case CLICKTOSTART:
+                help.updateglow();
+                break;
+
+            default:
+                break;
         }
-        else
-        {
-            switch (game.gamestate)
-            {
-                case PRELOADER:
-                    //Render
-                    preloaderrender(graphics, game, help);
-                    break;
 
-                case EDITORMODE:
-                    graphics.flipmode = false;
-                    //Input
-                    editorinput(key, graphics, game, map, obj, help, music);
-                    //Render
-                    editorrender(key, graphics, game, map, obj, help);
-                    //Logic
-                    editorlogic(key, graphics, game, obj, music, map, help);
-                    break;
-
-                case TITLEMODE:
-                    //Input
-                    titleinput(key, graphics, map, game, obj, help, music);
-                    //Render
-                    titlerender(graphics, map, game, obj, help, music);
-                    //Logic
-                    titlelogic(graphics, game, obj, help, music, map);
-                    break;
-
-                case GAMEMODE:
-                    if (map.towermode)
-                    {
-                        //Input
-					    gameinput(key, graphics, game, map, obj, help, music);
-                        //Render
-                        towerrender(graphics, game, map, obj, help);
-                        //Logic
-                        towerlogic(graphics, game, obj, music, map, help);
-                    }
-                    else
-                    {
-                        if (script.running)
-                        {
-                            script.run(key, graphics, game, map, obj, help, music);
-                        }
-
-                        //Input
-                        gameinput(key, graphics, game, map, obj, help, music);
-                        //Render
-                        gamerender(graphics,map, game, obj, help);
-                        //Logic
-                        gamelogic(graphics, game, obj, music, map,  help);
-                    }
-                    break;
-
-                case MAPMODE:
-                    //Input
-                    mapinput(key, graphics, game, map, obj, help, music);
-                    //Render
-                    maprender(graphics, game, map, obj, help);
-                    //Logic
-                    maplogic(graphics, game, obj ,music , map, help );
-                    break;
-
-                case TELEPORTERMODE:
-                    //Input
-                    if(game.useteleporter)
-                    {
-                        teleporterinput(key, graphics, game, map, obj, help, music);
-                    }
-                    else
-                    {
-                        if (script.running)
-                        {
-                            script.run(key, graphics, game, map, obj, help, music);
-                        }
-
-                        gameinput(key, graphics, game, map, obj, help, music);
-                    }
-                    //Render
-                    teleporterrender(graphics, game, map, obj, help);
-                    //Logic
-                    maplogic(graphics, game, obj, music, map, help);
-                    break;
-                case GAMECOMPLETE:
-                    //Input
-                    gamecompleteinput(key, graphics, game, map, obj, help, music);
-                    //Render
-                    gamecompleterender(graphics, game, obj, help, map);
-                    //Logic
-                    gamecompletelogic(graphics, game, obj, music, map, help);
-                    break;
-
-                case GAMECOMPLETE2:
-                    //Input
-                    gamecompleteinput2(key, graphics, game, map, obj, help, music);
-                    //Render
-                    gamecompleterender2(graphics, game, obj, help);
-                    //Logic
-                    gamecompletelogic2(graphics, game, obj, music, map, help);
-                    break;
-
-                case CLICKTOSTART:
-                    help.updateglow();
-                    break;
-
-                default:
-                    break;
+        #if !defined(__SWITCH__)
             }
-        }
+        #endif
 
         if (game.savemystats)
         {
